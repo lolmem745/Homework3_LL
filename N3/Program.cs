2852c977fb96c78ab196497d09b9987923efc694
lolmem745 <93839006+lolmem745@users.noreply.github.com>
2022-10-23 21:21:57 +0300

    Third problem solved

---
+++ N3/Program.cs
@@ -0,0 +1,13 @@
+﻿void cubeCounter(int num)
+{
+    for (int i = 1; i <= num; i++)
+    {
+        Console.Write($"{i * i * i}");
+        if (i != num)
+            Console.Write(", ");
+    }
+}
+
+Console.Write("Введите положительное число: ");
+
+cubeCounter(Convert.ToInt32(Console.ReadLine()));
